c72f1154d52d2210745dcb6fad106b1edd2bf61a
Felipe Molina <felipemolina.contato@gmail.com>
2023-04-25 23:06:28 -0300

    [feat] fragments e tab navigation

---
+++ app/src/main/java/com/cabret/projetoler/CarrinhoFragment.kt
@@ -0,0 +1,22 @@
+package com.cabret.projetoler
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class CarrinhoFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_carrinho, container, false)
+    }
+
+}
--- app/src/main/java/com/cabret/projetoler/MainActivity.kt
+++ app/src/main/java/com/cabret/projetoler/MainActivity.kt
@@ -4,15 +4,50 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    val AtualFragmentVal = 1;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val fragmentVal = 1
+        loadFragment(HomeFragment(), fragmentVal);
 
-        findViewById<Button>(R.id.btnNavegar).setOnClickListener{
-            val intent = Intent(this, LoginActivity::class.java);
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId){
+                R.id.inicio ->{
+                    loadFragment(HomeFragment(), 1)
+                    true
+                }
+                R.id.lista ->{
+                    loadFragment(ListaFragment(), 2)
+                    true
+                }
+                R.id.carrinho ->{
+                    loadFragment(CarrinhoFragment(),3)
+                    true
+                }
+                R.id.perfil ->{
+                    loadFragment(PerfilFragment(),4)
+                    true
+                }
 
-            startActivity(intent);
+                else -> {true}
+            }
         }
+    }
+
+    private  fun loadFragment(fragment: Fragment, fragmentVal: Int){
+        val transaction = supportFragmentManager.beginTransaction()
+        if(fragmentVal > AtualFragmentVal){
+            transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left);
+        }else{
+            transaction.setCustomAnimations(R.anim.slide_out_left, R.anim.slide_in_right);
+        }
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 }
